75e4bda2311ec9010150391444a6173914bd4265
jyotisko <jyotisko2006@gmail.com>
2021-01-29 23:39:51 +0530

    profile pic post now available

--- models/userModel.js
+++ models/userModel.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: ''
   },
+  profilePicUrl: {
+    type: String,
+    default: ''
+  },
   createdAt: {
     type: Date,
     default: Date.now
